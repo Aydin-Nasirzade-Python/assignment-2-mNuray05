bb9e02daefb9ef3e81f4f7ff112e369d0eb5fcb3
mNuray05 <162189328+mNuray05@users.noreply.github.com>
2024-04-07 17:42:52 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -2,20 +2,20 @@
 
 def main():
   
-l=int(input("Enter the wavelength in nm:"))
-if l>=380 or l<450:
+  l=int(input("Enter the wavelength in nm: ")
+  if l>=380 or l<450:
     print("The relevant color is Violet")
-elif l>=450 or l<495:
+  elif l>=450 or l<495:
     print("The relevant color is Blue")
-elif l>=495 or l<570:
+  elif l>=495 or l<570:
     print (" The relevant color is Green")
-elif l>=570 or l<590:
+  elif l>=570 or l<590:
     print(" The relevant color is Yellow")
-elif l>=590 or l<620:
+  elif l>=590 or l<620:
     print("The relevant color is Orange")
-elif l>=620 or l<750:
+  elif l>=620 or l<750:
     print("The relevant color is Red")
-else:
+  else:
     print ("Invalid input!")
   pass
 
