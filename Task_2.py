d0968adb70bd90720c7734e51f7a88eb62777986
mNuray05 <162189328+mNuray05@users.noreply.github.com>
2024-04-07 17:38:34 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,32 +1,32 @@
 #import libraries here
 
 def main():
-  if m=="March" or m=="April" or m=="May" or m=="June":
-      if (m=="March" and d<20) or (m=="June" and d>=21):
+  if month=="March" or month=="April" or month=="May" or month=="June":
+      if (month=="March" and d<20) or (month=="June" and d>=21):
           pass
       else:
-          print(m,d,"is in Spring")
+          print(f"{month} {day} is in Spring\n")
   else:
       pass
-  if m=="June" or m=="July" or m=="August" or m=="September":
-      if (m=="June" and d<21)or(m=="September" and d>=22):
+  if month=="June" or month=="July" or month=="August" or month=="September":
+      if (month=="June" and d<21)or(month=="September" and d>=22):
           pass
       else:
-          print(m,d,"is in Summer")
+          print(f"{month} {day} is in Summer\n")
   else:
       pass
-  if m=="October" or m=="November" or m=="December" or m=="September":
-      if (m=="September" and d<22) or(m=="December" and d>=21):
+  if month=="October" or month=="November" or month=="December" or month=="September":
+      if (month=="September" and d<22) or(month=="December" and d>=21):
           pass
       else:
-          print(m,d,"is in Fall")
+         print(f"{month} {day} is in Fall\n")
   else:
       pass
-  if m=="January" or m=="February" or m=="December" or m=="March":
-      if (m=="December" and d<21) or(m=="March" and d>=20):
+  if month=="January" or month=="February" or month=="December" or month=="March":
+      if (month=="December" and d<21) or(month=="March" and d>=20):
           pass
       else:
-          print(m,d,"is in Winter")
+         print(f"{month} {day} is in Winter\n")
   pass
 
 if __name__ == "__main__":
