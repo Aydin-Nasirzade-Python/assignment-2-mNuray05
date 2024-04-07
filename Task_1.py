5a363e55e442e717cc0ddfa042ed10180dc6cd95
mNuray05 <162189328+mNuray05@users.noreply.github.com>
2024-04-07 17:14:24 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,12 +2,12 @@
 
 def main():
     l=input("Enter a letter of the alphabet: ")
-    vowels=["a", "o", "u", "i", "e"]
+    vowels=["a", "o", "u", "i", "e","A", "O", "U", "I", "E"]
     if l in vowels:
         print("Entered alphabet is a vowel!")
-    elif l=="y":
+    elif l=="y" or l=="Y":
         print("Sometimes it is a vowel, and sometimes it is a consonant!")
-    else:
+    elif l in "qQwWrRtTpPsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM"
         print("Entered alphabet is a consonant!")
 
 if __name__ == "__main__":
